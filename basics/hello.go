a5b47ba3c7179f4eb2ddefc7fbf7f793e2e6bb98
Shag3b <180705089+Shag3b@users.noreply.github.com>
2025-03-21 16:37:52 +0200

    same

---
+++ basics/hello.go
@@ -0,0 +1,8 @@
+package basics
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello to new Lang")
+
+}
---
+++ basics/naming_conventions.go
@@ -0,0 +1,19 @@
+package basics
+
+func basics() {
+	//pascalCase
+	//ex.  CalculateArea,UserInfo
+	//use in structs,interfaces,enums
+
+	//snake_case
+	//ex.  user_id,first_name
+	// to filename
+
+	//UPPERCASE
+	//use in constants
+
+	//mixedCase
+	//ex. htmlDocument,isValid
+	//to init var                //if use to var donit mix it with snake
+
+}
